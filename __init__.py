702c0937424e926df64ee55aa027819cc30f20b9
Scott Davis <stdavis@utah.gov>
2017-10-02 16:42:40 -0600

    initial commit after plugin builder template

---
+++ __init__.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ AGRCGeocodingToolbox
+                                 A QGIS plugin
+ This plugin provides tools for geocoding addresses using AGRC's web services.
+                             -------------------
+        begin                : 2017-10-02
+        copyright            : (C) 2017 by AGRC
+        email                : stdavis@utah.gov
+        git sha              : $Format:%H$
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+ This script initializes the plugin, making it known to QGIS.
+"""
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load AGRCGeocodingToolbox class from file AGRCGeocodingToolbox.
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    #
+    from .geocoding_toolbox import AGRCGeocodingToolbox
+    return AGRCGeocodingToolbox(iface)
---
+++ test/test_resources.py
@@ -0,0 +1,44 @@
+# coding=utf-8
+"""Resources test.
+
+.. note:: This program is free software; you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation; either version 2 of the License, or
+     (at your option) any later version.
+
+"""
+
+__author__ = 'stdavis@utah.gov'
+__date__ = '2017-10-02'
+__copyright__ = 'Copyright 2017, AGRC'
+
+import unittest
+
+from PyQt4.QtGui import QIcon
+
+
+
+class AGRCGeocodingToolboxDialogTest(unittest.TestCase):
+    """Test rerources work."""
+
+    def setUp(self):
+        """Runs before each test."""
+        pass
+
+    def tearDown(self):
+        """Runs after each test."""
+        pass
+
+    def test_icon_png(self):
+        """Test we can click OK."""
+        path = ':/plugins/AGRCGeocodingToolbox/icon.png'
+        icon = QIcon(path)
+        self.assertFalse(icon.isNull())
+
+if __name__ == "__main__":
+    suite = unittest.makeSuite(AGRCGeocodingToolboxResourcesTest)
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(suite)
+
+
+
